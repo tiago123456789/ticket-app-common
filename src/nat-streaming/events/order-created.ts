de63e707030a61d5ef1f2ef122904ed401be9a91
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-09-09 21:58:37 -0300

    Add attributes in order created event

--- src/nat-streaming/events/order-created.ts
+++ src/nat-streaming/events/order-created.ts
@@ -2,6 +2,9 @@ import { Event } from "./event";
 
 export interface OrderCreated extends Event {
     id?: string;
+    status: string;
+    userId: string;
+    price: Number;
     expiration: Date;
     ticketId: string;
 }
