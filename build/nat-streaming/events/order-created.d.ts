945748148da224f01cfd0c6f2365c5a0b8fa6bd3
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-09-12 23:24:13 -0300

    Build project to take changes OrderCreated event

--- build/nat-streaming/events/order-created.d.ts
+++ build/nat-streaming/events/order-created.d.ts
@@ -1,6 +1,9 @@
 import { Event } from "./event";
 export interface OrderCreated extends Event {
     id?: string;
+    status: string;
+    userId: string;
+    price: Number;
     expiration: Date;
     ticketId: string;
 }
